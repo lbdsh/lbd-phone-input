315a2347b005930579e1ce7c78e5710fb0688adc
Antonino <antonino.testa@me.com>
2025-11-02 23:25:58 +0100

    refactor: remove esbuild dependency and simplify postbuild script
    - Removed esbuild from devDependencies in package.json.
    - Simplified postbuild script by replacing esbuild build calls with direct file copy for ESM and CJS outputs.
    - Updated vitest configuration to use jsdom instead of happy-dom for the testing environment.

--- scripts/postbuild.mjs
+++ scripts/postbuild.mjs
@@ -20,23 +20,8 @@ if (existsSync(assetsSrc)) {
   cpSync(cjsSource, resolve(cdnDest, 'lbd-phone-input.cjs'));
   cpSync(cssSource, resolve(cdnDest, 'lbd-phone-input.css'));
 
-  const esbuild = (await import('esbuild')).build;
-
-  await esbuild({
-    entryPoints: [esmSource],
-    outfile: resolve(cdnDest, 'lbd-phone-input.esm.min.js'),
-    minify: true,
-    bundle: false,
-    format: 'esm'
-  });
-
-  await esbuild({
-    entryPoints: [cjsSource],
-    outfile: resolve(cdnDest, 'lbd-phone-input.cjs.min.js'),
-    minify: true,
-    bundle: false,
-    format: 'cjs'
-  });
+  cpSync(esmSource, resolve(cdnDest, 'lbd-phone-input.esm.min.js'));
+  cpSync(cjsSource, resolve(cdnDest, 'lbd-phone-input.cjs.min.js'));
 
   cpSync(assetsSrc, resolve(cdnDest, 'assets'), { recursive: true });
 }
